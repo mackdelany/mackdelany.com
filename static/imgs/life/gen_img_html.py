3d533ded7f2e63eaeab4049525bf669659164e94
mackdelany <mackdelany@gmail.com>
2021-07-23 16:50:50 +1200

    random memory generator

---
+++ static/imgs/life/gen_img_html.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from airium import Airium
+
+PATH_TO_IMGS = 'static/imgs/life/'
+
+files = os.listdir('.')
+imgs = [f for f in files if '.jp' in f]
+
+
+def get_img_title(filename: str) -> str:
+    return filename.replace('_', ' ').split('.')[0]
+
+
+a = Airium()
+for img in imgs:
+    img_tile = get_img_title(img)
+    with a.div():
+        a.img(src=str(Path(PATH_TO_IMGS, img)), alt=img_tile, klass="memory-generator-img")
+        a(img_tile)
+
+html_str = str(a)
+print(html_str)
